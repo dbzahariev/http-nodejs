c66a36ef34eee27b53b53bfd7df7ac8b787426ef
Dimitar Zahariev <dbzahariev@gmail.com>
2022-12-04 11:13:55 +0200

    kk2

--- server.mjs
+++ server.mjs
@@ -3,7 +3,8 @@ import { createServer } from 'http';
 createServer((req, res) => {
   // res.write('Hello World3!');
   // res.json({ userName: "ramsess" })
-  res.setHeader('Content-Type', 'application/json');
-  res.json({ userName: "ramsess" })
+  // res.setHeader('Content-Type', 'application/json');
+  res.write(JSON.stringify({ userName: "ramsess" }))
   res.end();
 }).listen(process.env.PORT);
+
