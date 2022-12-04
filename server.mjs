16ce24a6f4c924100b988b2e4e4242beb96ff060
Dimitar Zahariev <dbzahariev@gmail.com>
2022-12-04 10:45:48 +0200

    Update server

--- server.mjs
+++ server.mjs
@@ -2,6 +2,6 @@ import { createServer } from 'http';
 
 createServer((req, res) => {
   // res.write('Hello World2!');
-  res.json([{ userName: "ramsess" }])
+  res.json({ userName: "ramsess" })
   res.end();
 }).listen(process.env.PORT);
