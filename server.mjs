56749e567b9c49b2c977458aa3e55b14205c6646
Dimitar Zahariev <dbzahariev@gmail.com>
2022-12-04 11:02:30 +0200

    Fix content type

--- server.mjs
+++ server.mjs
@@ -3,6 +3,7 @@ import { createServer } from 'http';
 createServer((req, res) => {
   // res.write('Hello World3!');
   // res.json({ userName: "ramsess" })
+  res.writeHead(200, { 'Content-Type': 'application/json' });
   res.write({ userName: "ramsess" })
   res.end();
 }).listen(process.env.PORT);
