d31dc1719e4043c2b95a330bc4631a6deb045206
Dimitar Zahariev <dbzahariev@gmail.com>
2022-12-04 10:54:56 +0200

    gg

--- server.mjs
+++ server.mjs
@@ -3,6 +3,6 @@ import { createServer } from 'http';
 createServer((req, res) => {
   // res.write('Hello World3!');
   // res.json({ userName: "ramsess" })
-  res.write(JSON.stringify({ userName: "ramsess" }))
+  res.write({ userName: "ramsess" })
   res.end();
 }).listen(process.env.PORT);
